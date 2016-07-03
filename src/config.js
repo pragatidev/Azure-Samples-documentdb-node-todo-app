7626861b1a1c2fa0ae828db0ae90c6c220646d50
pragatidev <pragatikunwer@gmail.com>
2016-07-03 13:03:13 -0700

    Changing config information for Azure

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 var config = {}
 
-config.host = process.env.HOST || "[URI for DocumentDB endpoint]";
-config.authKey = process.env.AUTH_KEY || "[Master key for DocumentDB]";
+config.host = process.env.HOST || "https://nodedbsample.documents.azure.com:443/";
+config.authKey = process.env.AUTH_KEY || "CkCU60xrUXtBgABHawG6I0tbCNoWijxwZF3lmL6TcVSGyriITpGyothdsDEfx5ym0buo2yCcpDaSjrd1P2757A==";
 config.databaseId = "ToDoList";
 config.collectionId = "Items";
 
